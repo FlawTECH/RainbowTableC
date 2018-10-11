a9eba81f18a9fa9ef487bc16b44e9a4bd9fef48e
flawtech <flawtech@null.net>
2018-10-11 09:10:25 +0200

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int hello;
+    int hellos[5] = {0,2,4,6,8};
+    char hellow[6] = "Hello";
+    hello = 3;
+    printf("%5s world !%d", hellow, hellos[2]);
+    return 0;
+}
