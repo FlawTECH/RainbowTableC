6830a7309c14a608bfdeac055515bf5144d49363
unknown <mathias.hanquiniaux@gmail.com>
2018-10-15 08:50:04 +0200

    Create File + Read & Write

--- src/main.c
+++ src/main.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../lib/sha256.h"
+#define LENGTH_MAX 10000
+#define LENGTH_STRING 12
+#define FILEPATH "bin/password.txt"
 
-int main()
-{
-    int hello;
-    int hellos[5] = {0,2,4,6,8};
-    char hellow[6] = "Hello";
-    hello = 3;
-    printf("%5s world !%d", hellow, hellos[2]);
-    system("pause");
-    return 0;
-}
+
+void createFile(char var[LENGTH_STRING])
+    {
+        FILE* file = NULL;
+        file = fopen(FILEPATH, "a+");
+        if (file != NULL)
+            {
+            printf("[*] Success : File open \n");           
+            fprintf(file, "%s \n", var);
+            fclose(file);
+            }
+        else
+            {
+            printf("[*] Error, impossible to open the file \n");
+            }       
+        return 0;
+    }
+
+
+void readfile(void)
+    {
+    FILE* file = NULL;
+    char chaine[LENGTH_MAX] = "";    
+    file = fopen(FILEPATH, "r");
+
+    if (file != NULL)
+    {
+         while (fgets(chaine, LENGTH_MAX, file) != NULL)
+            {
+                printf("[*] %s", chaine);
+            }
+            fclose(file);
+        }
+    }
+
+int main(int argc, char *argv[])
+    {
+        char stringToWrite [LENGTH_STRING] = "Something";
+        createFile(stringToWrite);
+        readfile();
+        system("pause");
+
+        return 0;
+    }
